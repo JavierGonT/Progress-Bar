5ac801fa93ee63172290b10a43498bdc0ff3e7ad
JavierGonzalez <javier-jose.gonzaleztoledo@atos.net>
2023-08-23 01:03:18 +0100

    Se actualiza el botón disabled al principio

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
-let currentActive = 0
+let currentActive = 1
 
 next.addEventListener('click', () => {
     currentActive++
@@ -33,10 +33,11 @@ function update() {
             circle.classList.remove('active')
         }
     })
+    
 
     const actives = document.querySelectorAll('.active')
 
-    /* progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%' */
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
     if(currentActive === 1) {
         prev.disabled = true
